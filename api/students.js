0b5f8dae2dac16c3cf8f61c1c80fad9d16a55cd9
Fahmiya006 <ffahmiya06@gmail.com>
2026-03-01 14:54:55 +0530

    Fixed serverless API

--- api/students.js
+++ api/students.js
@@ -1,5 +1,11 @@
-import students from "../backend/students.json";
-
 export default function handler(req, res) {
+  const students = [
+    {
+      id: 1,
+      name: "Fahmiya",
+      dept: "CSE"
+    }
+  ];
+
   res.status(200).json(students);
 }
